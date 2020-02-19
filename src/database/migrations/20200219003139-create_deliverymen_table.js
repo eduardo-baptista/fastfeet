e4a0dec3d0c70d3fc046cdda2afdff16aa006079
edubaptista7 <eduardofbaptista@gmail.com>
2020-02-18 21:43:21 -0300

    added migration for deliverymen and files tables

---
+++ src/database/migrations/20200219003139-create_deliverymen_table.js
@@ -0,0 +1,43 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('deliverymen', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      name: {
+        type: Sequelize.STRING(30),
+        allowNull: false,
+      },
+      avatar_id: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'files',
+          key: 'id',
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL',
+        allowNull: true,
+      },
+      email: {
+        type: Sequelize.STRING(100),
+        allowNull: false,
+        unique: true,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+
+  down: queryInterface => {
+    return queryInterface.dropTable('deliverymen');
+  },
+};
