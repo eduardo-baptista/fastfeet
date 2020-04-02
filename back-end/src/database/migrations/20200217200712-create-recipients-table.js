aac45bf0f0b80c1b0bbd13c4b45f1a2db4c5b2ae
edubaptista7 <eduardofbpatista@gmail.com>
2020-04-02 14:27:45 -0300

    change state column size on recipient

--- back-end/src/database/migrations/20200217200712-create-recipients-table.js
+++ back-end/src/database/migrations/20200217200712-create-recipients-table.js
@@ -24,7 +24,7 @@ module.exports = {
         allowNull: false,
       },
       state: {
-        type: Sequelize.STRING(2),
+        type: Sequelize.STRING(30),
         allowNull: false,
       },
       city: {
