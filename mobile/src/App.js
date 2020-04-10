3e47722c196689c83e2168e2435c201ec12d4b44
edubaptista7 <eduardofbaptista@gmail.com>
2020-04-09 22:44:07 -0300

    Mobile app First Commit

---
+++ mobile/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text>My beatifull app</Text>
+    </View>
+  );
+};
+
+export default App;
